4e2d5b39ce4b8769e4d1f490ee459baada36972f
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-05 15:52:22 +0530

    Add files via upload

---
+++ Defaultandstatic.java
@@ -0,0 +1,15 @@
+﻿package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n,i;
+        n = in.nextInt();
+        String [] l = new String[n+1];
+        for(i=0;i<n+1;i++)
+            l[i]=in.nextLine();
+        for(String num : l)
+            System.out.println(num);
+    }
+}
